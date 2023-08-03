afca00262ea289710ec14fefb31c11d96ddf9725
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-08-03 15:48:50 -0300

    feat: added useGetTicket hook for fetching user's ticket information

--- src/contexts/TicketContext.js
+++ src/contexts/TicketContext.js
@@ -1,10 +1,29 @@
 import { useState, createContext } from 'react';
+//import useGetTicket from '../hooks/api/useGetTicket';
 
 const TicketContext = createContext();
 
 export function TicketProvider({ children }) {
   const [ticketData, setTicketData] = useState({});
   
+  //const { ticket } = useGetTicket();
+
+  // useEffect(() => {
+  //   if (ticket) {
+  //     setTicketData(ticket);
+  //   }
+  // }, [ticket]);
+
+  // function updateTicket() {
+  //   const { ticket } = useGetTicket();
+
+  //   useEffect(() => {
+  //     if (ticket) {
+  //       setTicketData(ticket);
+  //     }
+  //   }, [ticket]);
+  // }
+
   return (
     <TicketContext.Provider value={{ ticketData, setTicketData }}>
       {children}
